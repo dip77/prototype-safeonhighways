86d61ec5563f4e8dc680a96b4db4541ce4f46830
dip halani <dip.thakkar777@gmail.com>
2017-06-01 20:08:04 -0700

    prototype with location as da-iict (RC)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers:[Geolocation]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private geolocation: Geolocation) {
 
-  }
+this.geolocation.getCurrentPosition().then((resp) => {
+ // resp.coords.latitude
+ // resp.coords.longitude
 
+}).catch((error) => {
+  console.log('Error getting location', error);
+});
+
+let watch = this.geolocation.watchPosition();
+watch.subscribe((data) => {
+	alert("speed:"+ data.coords.speed+
+		" and  meters "+this.measure(data.coords.latitude,data.coords.longitude,23.188996,72.628445))
+
+ // data can be a set of coordinates, or an error (if an error occurred).
+ // data.coords.latitude
+ // data.coords.longitude
+});
+}
+ measure(lat1, lon1, lat2, lon2){  // generally used geo measurement function
+    var R = 6378.137; // Radius of earth in KM
+    var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+    var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+    Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d * 1000; // meters
+}
 }
